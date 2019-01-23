517eb48976741ab155193ff9ba9c4eb2ef9bc58e
nsneeraj <46974049+nsneeraj@users.noreply.github.com>
2019-01-24 00:36:36 +0530

    Initial commit

---
+++ src/mypractice/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mypractice;
+
+/**
+ * @author Hp_Lappy
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
